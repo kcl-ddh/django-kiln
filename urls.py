c7d82464f8cf7008e02416a2206227d71eb03bf1
Jamie Norrish <jamie@artefact.org.nz>
2014-01-07 13:07:49 +1300

    Removed unwanted code. Replaced lxml with ElementTree to reduce dependencies. Simplified Kiln URL extraction.

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('kiln.views',
-                       url(r'^(?P<parameters>[^?]*)/$', 'process',
+                       url(r'^(?P<kiln_url>[^?]*)$', 'process',
                            {'template': 'process.html'}, name='kiln'),
                        )
--- views.py
+++ views.py
@@ -1,52 +1,43 @@
-from counter.views import log_request
 from django.conf import settings
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.decorators.gzip import gzip_page
-from lxml import etree
 
-import re
+import xml.etree.ElementTree as ET
 import requests
 
 
 @gzip_page
-def process(request, parameters, template):
+def process(request, kiln_url, template):
     """Processes a request that needs to be sent to Kiln."""
-    # constructs the path to the Kiln server
-    path = re.sub(r'^.*?' + settings.KILN_CONTEXT_PATH, '/', request.path)
+    # Construct the URL for the request to the Kiln server.
     query_string = request.META.get('QUERY_STRING')
     query_string = '?' + query_string if query_string else ''
-    url = settings.KILN_BASE_URL + path + query_string
+    kiln_base_url = settings.KILN_BASE_URL
+    if kiln_base_url[-1] != '/':
+        kiln_base_url = kiln_base_url + '/'
+    url = kiln_base_url + kiln_url + query_string
 
-    # sends the request to Kiln
+    # Send the request to Kiln.
     r = requests.get(url)
-    response = r.text
-    # creates a new XML tree from the response
-    root = etree.fromstring(response)
+    response = r.text.encode('utf-8')
+    # Create a new XML tree from the response.
+    root = ET.fromstring(response)
 
-    # element names that are expected in the response
+    # Element names that are expected in the response.
     element_names = ['title', 'css', 'js', 'header', 'breadcrumbs', 'content']
 
-    # parameters dictionary to be passed to the template
+    # Parameters to be passed to the template.
     params = {}
-
-    # title of the page to log the request
-    title = '*empty*'
 
     for element_name in element_names:
         params[element_name] = ''
         element = root.find(element_name)
         if len(element):
             for child in element:
-                params[element_name] += etree.tostring(child)
+                params[element_name] += ET.tostring(child, encoding='utf-8')
         elif element.text:
             params[element_name] = element.text
-
-        if element_name == 'title':
-            title = element.text
-
-    # logs the request
-    log_request(request, title)
 
     return render_to_response(template, params,
                               context_instance=RequestContext(request))
